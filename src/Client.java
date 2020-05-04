89ca57c7be9ea7f515c9acde45dc81dc23473def
S4RPYK3IVl5 <andreysaprykin@mail.ru>
2020-05-04 12:04:15 +0400

    inital commit

---
+++ src/Client.java
@@ -0,0 +1,29 @@
+import java.io.*;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        var socket = new Socket("localhost", 6868);
+
+        var outputStream = socket.getOutputStream();
+
+        var pw = new PrintWriter(outputStream, true);
+        var scanner = new Scanner(System.in);
+
+        var inputStream = socket.getInputStream();
+        var br = new BufferedReader(new InputStreamReader(inputStream));
+
+        String res;
+        do {
+            System.out.print("Write your numbers by comma : ");
+            String text = scanner.nextLine();
+            pw.println(text);
+
+            res = br.readLine();
+            System.out.println(res);
+        } while (!res.equals("exit"));
+
+    }
+}
---
+++ src/Server.java
@@ -0,0 +1,44 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.ServerSocket;
+
+public class Server {
+    public static void main(String[] args) throws IOException {
+
+        ServerSocket serverSocket = new ServerSocket(6868);
+
+        while (true) {
+
+            var socket = serverSocket.accept();
+
+            var outputStream = socket.getOutputStream();
+            var pw = new PrintWriter(outputStream, true);
+
+            var inputStream = socket.getInputStream();
+            var isr = new BufferedReader(new InputStreamReader(inputStream));
+
+            System.out.println("The client was accepted");
+            System.out.println("Processing...");
+
+            String inputText;
+            do {
+                inputText = isr.readLine();
+                String[] numbersStr = inputText.split(",");
+                Integer[] integersInt = {Integer.valueOf(numbersStr[0]), Integer.valueOf(numbersStr[1])};
+                pw.println("The noc is: " + noc(integersInt[0], integersInt[1])
+                        + " if you wanna to end with server, write 'exit'");
+            } while (!inputText.equals("exit"));
+            pw.println("exit");
+
+        }
+
+    }
+
+    private static int noc(int x, int y){
+        if(y == 0) return x;
+        else return noc(y, x%y);
+    }
+
+}
